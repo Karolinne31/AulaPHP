1bf5adcf5d7983f8a5f6b31fbfd9592fa976378d
Karolinne31 <karolinnefrancisco33@gmail.com>
2025-10-23 14:19:26 -0300

    23/10

---
+++ AULABD/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Etec SJRP</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+
+    <?php include "menu.php" ?>
+
+    <div class="conteudo">
+        <h3>Integração de PHP com MySql</h3>
+        <p>Na aula de hoje, vamos estudar como integrar o PHP com o MySQL</p>
+    </div>
+</body>
+
+</html>
