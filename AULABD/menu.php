f5870d2c90ba88f99aada3e4fad5bf42022aa6ee
Karolinne31 <karolinnefrancisco33@gmail.com>
2025-10-30 11:23:39 -0300

    30/10

--- AULABD/menu.php
+++ AULABD/menu.php
@@ -8,4 +8,8 @@
             <a href="aluno_cadastro.php">Cadastrar</a>
             <a href="#">Consultar</a>
         </div>
+            <a href="professor_cadastro.php">Prof. Cadastrar </a>
+            <a href="professor_consulta.php"> Prof. Consultar </a>
+        <div>
+
     </div>
